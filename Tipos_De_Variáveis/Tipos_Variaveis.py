89b067b8f0022e045602bf4b863746e6a422c7cd
Rosana <rosana_svieira@hotmail.com>
2023-09-14 17:19:04 -0300

    Tipos_Variaveis

---
+++ Tipos_De_Variáveis/Tipos_Variaveis.py
@@ -0,0 +1,6 @@
+# Faça um programa que receba o nome de um aluno e quanto ele tirou na prova de programaçao, depois imprima
+# em apenas uma linha o nome no mod title() e quanto a pessoa tirou na prova. Ex: Ana Carolina tirou 8
+
+nomeAluno = input('Digite o nome do aluno: ')
+nota = input('Digite a nota: ')
+print(f'{nomeAluno.title()} tirou {nota}')
